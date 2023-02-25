5e428bcc9c2998b4c3f161bef51d60501e515565
dexter1998wild <38758158+dexter1998wild@users.noreply.github.com>
2023-02-25 16:30:51 -0500

    Update find_phone.py

--- find_phone.py
+++ find_phone.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import processor
 import sys
 
@@ -8,15 +7,5 @@ except:
     raise Exception('No file found')
     
 test_out = processor.phone_finder(image)
-=======
-import processor
-import sys
 
-try:
-    image = sys.argv[1]
-except:
-    raise Exception('No file found')
-    
-test_out = processor.phone_finder(image)
->>>>>>> 098a205324abb03f42190b81439820c9e857f4df
 print(str(test_out[0]) + ' ' + str(test_out[1]) )
